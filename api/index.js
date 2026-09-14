20730dc18e5b86776d98e248c5af1a5215314a1c
motormaniac <darrienlee626@gmail.com>
2026-09-14 19:26:58 -0400

    Make post request align with previous post requests

--- api/index.js
+++ api/index.js
@@ -25,5 +25,10 @@ app.post('/', (req, res) => {
 
 // ========= Write your own post request here to do whatever you want ========
 app.post('/hello-world', (resq, res) => {
+  if (!req.body || !req.body.name) {
+    res.status(400).send("Missing a name in the request body!");
+    return;
+  }
+  users.push(req.body.name);
   res.send("Hello World!");
 })
